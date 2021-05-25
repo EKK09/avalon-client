3e0be710d832aefa2394a7e8d35715ad0640f6f4
wei <xe856423@gmail.com>
2021-05-25 18:16:51 +0800

    修改遊戲 vuex 預設值

--- src/store/game/state.js
+++ src/store/game/state.js
@@ -4,63 +4,63 @@ import { GAME_ROLE } from 'src/constants/role.js';
 //  role: string;
 // }
 
-const fooPlayers = [
-  {
-    name: 'david',
-    role: GAME_ROLE.MERLIN,
-  },
-  {
-    name: 'Terry',
-    role: GAME_ROLE.UNKNOWN,
-  },
-  {
-    name: '啞色的忠誠',
-    role: GAME_ROLE.GOOD,
-  },
-  {
-    name: '我是好人',
-    role: GAME_ROLE.MINION,
-  },
-  {
-    name: 'Jerry',
-    role: GAME_ROLE.PERCIVAL,
-  },
-  {
-    name: '海豹',
-    role: GAME_ROLE.EVIL,
-  },
-  {
-    name: '張莉莉張',
-    role: GAME_ROLE.SERVANT,
-  },
-  {
-    name: '8228',
-    role: GAME_ROLE.MORDRED,
-  },
-  {
-    name: '我是爪牙',
-    role: GAME_ROLE.OBERON,
-  },
-  {
-    name: '魔甘吶',
-    role: GAME_ROLE.UNKNOWN,
-  },
-];
+// const fooPlayers = [
+//   {
+//     name: 'david',
+//     role: GAME_ROLE.MERLIN,
+//   },
+//   {
+//     name: 'Terry',
+//     role: GAME_ROLE.UNKNOWN,
+//   },
+//   {
+//     name: '啞色的忠誠',
+//     role: GAME_ROLE.GOOD,
+//   },
+//   {
+//     name: '我是好人',
+//     role: GAME_ROLE.MINION,
+//   },
+//   {
+//     name: 'Jerry',
+//     role: GAME_ROLE.PERCIVAL,
+//   },
+//   {
+//     name: '海豹',
+//     role: GAME_ROLE.EVIL,
+//   },
+//   {
+//     name: '張莉莉張',
+//     role: GAME_ROLE.SERVANT,
+//   },
+//   {
+//     name: '8228',
+//     role: GAME_ROLE.MORDRED,
+//   },
+//   {
+//     name: '我是爪牙',
+//     role: GAME_ROLE.OBERON,
+//   },
+//   {
+//     name: '魔甘吶',
+//     role: GAME_ROLE.UNKNOWN,
+//   },
+// ];
 
 export default function state() {
   return {
     roomId: '',
-    user: 'david',
-    role: GAME_ROLE.MERLIN,
-    leader: 'david',
-    playerList: fooPlayers,
+    user: '',
+    role: GAME_ROLE.UNKNOWN,
+    leader: '',
+    playerList: [],
     taskTeamList: [],
     taskResultList: [undefined, undefined, undefined, undefined, undefined],
     message: '',
     status: '',
     selectedTaskTeamList: [],
-    teamSize: 4,
-    round: 3,
+    teamSize: 0,
+    round: 0,
     webSocket: null,
   };
 }

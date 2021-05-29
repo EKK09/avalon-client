ccfa5b23925cc22ff076a125c2e334eabb84bdd6
wei <xe856423@gmail.com>
2021-05-29 12:21:47 +0800

    調整 haeder 樣式

--- src/components/GameHeader.vue
+++ src/components/GameHeader.vue
@@ -4,47 +4,58 @@
     bordered
     class="bg-blue-grey-9"
   >
-    <q-toolbar class="q-pa-sm">
-      <div
-        style="width: 100px"
-      >
-        <div class="text-bold fz-md ellipsis full-width">
-          房號: {{ roomId }}
-        </div>
-        <q-img
-          :src="userRoleImage"
-          width="100px"
-          height="140px"
-        />
-      </div>
-      <q-toolbar-title class="flex flex-center q-pa-sm">
+    <q-item class="q-pa-sm">
+      <q-item-section avatar>
         <div
-          v-for="(result, index) in taskResultList"
-          :key="index"
-          class="flex flex-center"
+          style="width: 100px"
         >
-          <q-avatar
-            size="65px"
-            font-size="26px"
-            color="black"
-            text-color="white"
-            class="q-mx-sm"
-          >
-            <img
-              v-if="result === false"
-              src="score_fail.jpg"
-            >
-            <img
-              v-else-if="result === true"
-              src="score_success.jpg"
-            >
-            <template v-else-if="result === undefined">
-              任務{{ index + 1 }}
-            </template>
-          </q-avatar>
+          <div class="text-bold fz-md ellipsis full-width">
+            房號: {{ roomId }}
+          </div>
+          <q-img
+            :src="userRoleImage"
+            width="100px"
+            height="140px"
+          />
+          <div class="text-bold ellipsis full-width text-center">
+            David
+          </div>
         </div>
-      </q-toolbar-title>
-    </q-toolbar>
+      </q-item-section>
+
+      <q-item-section>
+        <div class="flex flex-center">
+          <div
+            v-for="(result, index) in taskResultList"
+            :key="index"
+            class="flex flex-center"
+          >
+            <q-avatar
+              size="65px"
+              font-size="26px"
+              color="black"
+              text-color="white"
+              class="q-mx-sm text-no-wrap"
+            >
+              <img
+                v-if="result === false"
+                src="score_fail.jpg"
+              >
+              <img
+                v-else-if="result === true"
+                src="score_success.jpg"
+              >
+
+              <template
+                v-else-if="result === undefined"
+              >
+                任務{{ index + 1 }}
+              </template>
+            </q-avatar>
+          </div>
+        </div>
+      </q-item-section>
+    </q-item>
   </q-header>
 </template>
 
@@ -55,7 +66,7 @@ export default {
   name: 'GameHeader',
   computed: {
     ...mapGetters('game', ['userRoleImage']),
-    ...mapState('game', ['roomId', 'taskResultList']),
+    ...mapState('game', ['roomId', 'taskResultList', 'user']),
   },
 };
 </script>
